b6f0a6baeeac5a0f2b9ef74274ce849d4f3ca31c
Prady27 <pradyumnchavan9@gmail.com>
2026-05-03 22:31:05 +0530

    initial project setup with package structure

---
+++ src/main/java/com/foodapp/food_delivery/model/User.java
@@ -0,0 +1,27 @@
+package com.foodapp.food_delivery.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+    private String password;
+    @Column(unique = true, nullable = false)
+    private String email;
+    private LocalDate createdAt;
+
+}
